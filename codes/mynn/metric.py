e25a61cece0f859e5452e2930315b5bf6f1bdda4
yykd0 <166713520+yykd0@users.noreply.github.com>
2026-05-24 20:48:10 +0800

    Add core package files

---
+++ codes/mynn/metric.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def accuracy(preds, labels):
+    """
+    Metric for MNIST.
+    preds : [batch, D]
+    labels : [batch, ]
+    """
+    assert preds.shape[0] == labels.shape[0]
+
+    predict_label = np.argmax(preds, axis=-1)
+    
+    return (predict_label == labels).sum() / preds.shape[0]
